b7b9ea9972b2b59a586726a5b90882198e7bad95
Aleksandr Panin <mia7007@yandex.ru>
2023-05-28 09:35:36 +0200

    make plz get in registry not mutable

--- src/registry.rs
+++ src/registry.rs
@@ -8,19 +8,14 @@ pub struct Registry<T> {
 }
 
 impl<T> Registry<T> {
-    // TODO remove mut
-    pub fn get(&mut self, t: ID) -> Option<&T> {
-        self._search(t).and_then(|x| x.as_ref())
+    pub fn get(&self, t: ID) -> Option<&T> {
+        let i = self.map.partition_point(|x| x.0 < t);
+        let ok = i != self.map.len() && self.map[i].0 == t;
+        if ok { self.map[i].1.as_ref() } else { None }
     }
 
     pub fn get_mut(&mut self, t: ID) -> Option<&mut T> {
-        self._search(t).and_then(|x| x.as_mut())
-    }
-
-    fn _search(&mut self, t: ID) -> Option<&mut Option<T>> {
-        let i = self.map.partition_point(|x| x.0 < t);
-        let ok = i != self.map.len() && self.map[i].0 == t;
-        if ok { Some(&mut self.map[i].1) } else { None }
+        self._search_mut(t).and_then(|x| x.as_mut())
     }
 
     pub fn append(&mut self, val: T) -> ID {
@@ -32,12 +27,18 @@ impl<T> Registry<T> {
     }
 
     pub fn erase(&mut self, t: ID) { 
-        let x = self._search(t);
+        let x = self._search_mut(t);
         if x.is_none() { return; }
         *x.unwrap() = None;
         self.size -= 1; 
         if self.size < self.map.len() / 2 {
             self.map.retain(|x| x.1.is_some());
         }
+    }
+
+    fn _search_mut(&mut self, t: ID) -> Option<&mut Option<T>> {
+        let i = self.map.partition_point(|x| x.0 < t);
+        let ok = i != self.map.len() && self.map[i].0 == t;
+        if ok { Some(&mut self.map[i].1) } else { None }
     }
 }
