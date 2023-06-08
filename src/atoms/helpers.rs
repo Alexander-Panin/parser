20cb8f02729cc636a49a00b32987da2063befaf4
Aleksandr Panin <mia7007@yandex.ru>
2023-06-08 12:31:17 +0200

    use plz the same name after unwrap

--- src/atoms/helpers.rs
+++ src/atoms/helpers.rs
@@ -1,11 +1,11 @@
 use crate::atoms::Node;
 use std::rc::Rc;
 
-pub fn tree_length(op: Option<Rc<Node>>) -> usize {
-    if op.is_none() {
+pub fn tree_length(node: Option<Rc<Node>>) -> usize {
+    if node.is_none() {
         return 0;
     }
-    let node = op.unwrap();
+    let node = node.unwrap();
     let a = tree_length(node.ok.as_ref().cloned());
     let b = tree_length(node.err.as_ref().cloned());
     std::cmp::max(a, b) + 1
