ba39618a52638182e27ff05f8cad92ad27ab7522
Aleksandr Panin <mia7007@yandex.ru>
2023-06-11 18:49:55 +0200

    add plz tests for

---
+++ src/tests/function.js
@@ -0,0 +1,18 @@
+function foo() {
+    function boo() {
+        var x = 5;
+        while (true) {}
+        function f() { 
+            const x = 5+5;
+            return 42;
+        }
+        const x = 5+5;
+        let goo = [{}];
+    }
+
+    function f() { }
+    function g() { }
+    function moo() { var x = 42; }
+}
+
+function more() {}
